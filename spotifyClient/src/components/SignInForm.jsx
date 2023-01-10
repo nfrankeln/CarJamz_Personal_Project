ecfdf79f4d70f1c0f61c3bdfd254d56a7753eaae
nfrankeln <ntnyfranky@gmail.com>
2023-01-10 13:24:53 -0600

    minoir changes to login css

--- spotifyClient/src/components/SignInForm.jsx
+++ spotifyClient/src/components/SignInForm.jsx
@@ -1,5 +1,21 @@
+import './SignInForm.css'
 export default function SignInForm(){
     return(
-        <div>SignIn</div>
+        <div id="sign-in-form-container">
+            <form id="sign-in-form">
+            <div className='input-wrapper'>
+                <label htmlFor="email">Email</label>
+                <input id='email' type="email"/>
+                </div>
+
+                <div className='input-wrapper'>
+                    <label htmlFor="password">Password</label>
+                <input id='password' type="text"/>
+                </div>
+                <div id='sign-up-button-wrapper'>
+                <button id='sign-up-button'>Login</button>
+                </div>
+            </form>
+        </div>
     )
 }
