94f0e5c58095f73e6bdb2acf46fc72375bb73242
nfrankeln <ntnyfranky@gmail.com>
2023-03-05 17:00:15 -0600

    added login and logout urls and views

--- spotifyServer/authorization/urls.py
+++ spotifyServer/authorization/urls.py
@@ -4,6 +4,9 @@ app_name="autherization"
 urlpatterns = [
     path('', views.index),
     path('api/register/',views.register),
+    path('api/login/',views.user_login),
+    path('api/logout/',views.user_logout),
+    path('api/is_authenticated/',views.is_authenicated),
     re_path(r'.*', views.index)
 ]
 
--- spotifyServer/authorization/views.py
+++ spotifyServer/authorization/views.py
@@ -1,20 +1,51 @@
 from django.shortcuts import render
+from django.conf import settings
 from rest_framework.decorators import api_view
+from django.contrib.auth import authenticate,login,logout
 from authorization.models import AppUser as User
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 def index(request):
     file = open('static/index.html').read()
     resp = HttpResponse(file)
     return resp
 
+
 @api_view(['POST'])
 def register(request):
-    print(request.data)
-    return HttpResponse("hello")
-    # try:
-    #     User.objects.create_user(**request.data['signUpData'])
-    #     return HttpResponse("User Created")
-    # except Exception as e:
-    #     print(e)
-    #     return HttpResponse("User Creation Failed")
+    try:
+        User.objects.create_user(**request.data['signUpData'])
+        return HttpResponse("User Created")
+    except Exception as e:
+        print(e)
+        return HttpResponse("User Creation Failed")
+
+@api_view(['POST'])
+def user_login(request):
+    http_request = request._request
+    email = request.data['loginData']['email']
+    password = request.data['loginData']['password']
+    # email = request.POST['email']
+    # password = request.POST['password']
+    user = authenticate(http_request, email=email, password=password)
+    if user is not None:
+        login(http_request, user)
+    #     TODO Redirect to a success page.
+    else:
+        print("user not found")
+    #     TODO Return an 'invalid login' error message.
+    #     ...
+    return HttpResponse('ok')
+@api_view(['POST'])
+def user_logout(request):
+    print("here")
+    logout(request)
+    return HttpResponse("logged out")
+@api_view(['GET'])
+def is_authenicated(request):
+    print(request.user.is_authenticated)
+    print(request.user)
+    response_data = {
+        "authenticated": request.user.is_authenticated
+    }
+    return JsonResponse(response_data)
