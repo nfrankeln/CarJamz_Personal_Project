04f10ea2f228ef3c8c04737547f06c182094bf67
nfrankeln <ntnyfranky@gmail.com>
2023-03-10 15:32:06 -0600

    added conditional use effect with api call to profile page

--- spotifyClient/src/pages/ProfilePage.jsx
+++ spotifyClient/src/pages/ProfilePage.jsx
@@ -1,16 +1,20 @@
 import axios from 'axios'
 import { useEffect } from 'react'
 export default function ProfilePage(props){
-    function getUrl(){
+    function authorizeSpotify(){
         axios.get('api/spotify/authorize/')
         .then((response)=>window.location.replace(response.data.url))
 
     }
-    console.log(props)
+    function userTopTenSongs(){
+        axios.get('api/spotify/top-songs/')
+        .then(response => console.log(response))
+    }
+    useEffect(()=>{
+        if(props.spotifyAuthorized){userTopTenSongs()}},[])
     return(
     <div>
-    <p>ProfilePage</p>
-    <button onClick={()=>getUrl()} >Connect to Spotify</button>
+    {props.spotifyAuthorized?<p>Your top 10</p>:<button onClick={()=>authorizeSpotify()} >Connect to Spotify</button>}
     </div>
         
     )
