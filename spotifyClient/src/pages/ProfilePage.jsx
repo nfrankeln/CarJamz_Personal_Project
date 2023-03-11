e2d79108fb6d87080c3821c7d0760d49026bdee3
nfrankeln <ntnyfranky@gmail.com>
2023-03-11 02:10:04 -0600

    moved playlist, useeffect, and use state into own component

--- spotifyClient/src/pages/ProfilePage.jsx
+++ spotifyClient/src/pages/ProfilePage.jsx
@@ -2,25 +2,17 @@ import axios from 'axios'
 import { useEffect, useState } from 'react'
 import Playlist from '../components/Playlist'
 export default function ProfilePage(props){
-    const [topTenSongs,setTopTenSongs] = useState(false)
+    
     function authorizeSpotify(){
         axios.get('api/spotify/authorize/')
-        .then((response)=>window.location.replace(response.data.url))
-
-    }
-    function userTopTenSongs(){
-        axios.get('api/spotify/top-songs/')
-        .then(response => {console.log(response.data.items);setTopTenSongs(response.data.items)})
-    }
-    useEffect(()=>{
-        if(props.spotifyAuthorized){userTopTenSongs()}},[])
+        .then((response)=>window.location.replace(response.data.url))}
+        
     return(
-    <div>
+    <div >
     {props.spotifyAuthorized?
     <div>
-    {topTenSongs?
-    <Playlist songs={topTenSongs}/>
-    :null}
+
+    <Playlist spotifyAuthorized={props.spotifyAuthorized}/>
     </div>
     :<button onClick={() => authorizeSpotify()}>Connect to Spotify</button>}
     </div>
